dadb7b44a4a1a29196d26d37a7f6a870a1c0a2c8
CarlosFelino <calexandrebr142@gmail.com>
2023-09-23 19:54:21 -0300

    Aula 3 aprendendo método

---
+++ java-anatomia-classes/src/MinhaClasse2.java
@@ -0,0 +1,13 @@
+public class MinhaClasse2 {
+    public static void main (String[] args){
+        String primeiroNome = "Carlos";
+        String segundoNome = "Alexandre";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
